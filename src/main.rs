be0fef423f76a025135845e25248cc0281914a95
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2020-01-17 16:53:04 +0100

    S3 upload works.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use log::{info};
 use std::thread;
-use crate::watcher::FileWatcher;
+//use crate::watcher::FileWatcher;
 use crossbeam_channel::unbounded;
 use crate::uploader::Uploader;
 
 mod database;
 mod error;
 mod uploader;
-mod watcher;
+//mod watcher;
 
 fn main() {
     setup_logger().unwrap();
     info!("Starting S3 File Sync...");
 
     let (upload_tx, upload_rx) = unbounded();
-    let watcher_db = database::Database::open("db.sqlite3").unwrap();
-
-    let file_watcher = FileWatcher::new(&["."], 2, watcher_db, upload_tx).unwrap();
-
-    let watcher_thread = thread::spawn(move || {
-        file_watcher.run();
-    });
+//    let watcher_db = database::Database::open("db.sqlite3").unwrap();
+//
+//    let file_watcher = FileWatcher::new(&["."], 2, watcher_db, upload_tx).unwrap();
+//
+//    let watcher_thread = thread::spawn(move || {
+//        file_watcher.run();
+//    });
 
     let uploader_db = database::Database::open("db.sqlite3").unwrap();
     let uploader = Uploader::new(
@@ -28,9 +28,9 @@ fn main() {
         "eu-west-3",
         upload_rx,
         uploader_db,
-    ).unwrap();
+    );
 
-    watcher_thread.join().expect("Failed to join watcher thread.");
+//    watcher_thread.join().expect("Failed to join watcher thread.");
 
 }
 
--- src/uploader.rs
+++ src/uploader.rs
@@ -1,16 +1,30 @@
 extern crate rusoto_core;
 extern crate rusoto_s3;
-extern crate tokio;
 
+use std::error::Error as StdError;
+use std::io::{Read, Error as IOError};
+use std::fmt;
+use std::fs::File as FSFile;
+use std::result::Result as StdResult;
 use std::str::FromStr;
 
 use crossbeam_channel::Receiver;
-use log::{info};
-use rusoto_core::{ByteStream, Region};
-use rusoto_s3::{S3Client, S3, PutObjectRequest};
+use log::{info, warn, error};
+use rusoto_core::{ByteStream, Region, RusotoError};
+use rusoto_s3::{
+    S3Client,
+    S3,
+    PutObjectRequest,
+    UploadPartRequest,
+    AbortMultipartUploadRequest,
+    CreateMultipartUploadRequest,
+    CreateMultipartUploadError,
+    CompletedMultipartUpload,
+    CompleteMultipartUploadRequest,
+};
 
 use crate::database::{Database, File};
-use crate::error::{Result};
+use self::rusoto_s3::CompletedPart;
 
 
 pub struct Uploader {
@@ -18,37 +32,171 @@ pub struct Uploader {
     upload_rx: Receiver<File>,
     db: Database,
     s3_client: S3Client,
+    request_payer: Option<String>,
+    part_size: usize,
 }
 
 
 impl Uploader {
-    pub fn new(bucket_name: &str, region_name: &str, upload_rx: Receiver<File>, db: Database) -> Result<Uploader> {
+    pub fn new(bucket_name: &str, region_name: &str, upload_rx: Receiver<File>, db: Database) -> Uploader {
         let region = Region::from_str(region_name).unwrap();
         let s3_client = S3Client::new(region);
         let bucket_name: String = bucket_name.into();
 
-        Ok(Uploader {
+        Uploader {
             bucket_name,
             upload_rx,
             db,
             s3_client,
-        })
+            request_payer: None,
+            part_size: 1024*1024*100,
+        }
     }
 
-    pub fn test(&self) {
+    pub fn upload_file(&self, filename: &str) -> Result<()> {
+        let upload_id = self.create_multipart_upload(filename)?;
 
+        if let Ok(c_mp_u) = self.upload_file_parts(filename, &upload_id) {
+            if let Ok(()) = self.complete_multipart_upload(filename, c_mp_u, &upload_id) {
+                info!("Completed upload of {}", filename);
+                return Ok(())
+            }
+        }
+        self.abort_multipart_upload(filename, &upload_id);
+        Err(Error::CompleteMultipartUpload)
+    }
 
-        let p_o_r = PutObjectRequest {
-            body: Some(byte_stream.into()),
-            bucket: self.bucket_name.clone(),
-            key: "copying".to_owned(),
-            ..Default::default()
-        };
+    fn upload_file_parts(&self, filename: &str, upload_id: &str) -> Result<CompletedMultipartUpload> {
+        let mut file = FSFile::open(filename).unwrap();
 
-        let put_result = self.s3_client.put_object(p_o_r).sync();
-        match put_result {
-            Ok(p_o_o) => info!("PutObjectOutput: {:?}", p_o_o),
-            Err(err) => info!("PutObjectError: {:?}", err),
+        let mut part_number = 1;
+
+        let mut completed_parts: Vec<CompletedPart> = Vec::new();
+
+        loop {
+            let mut buffer = vec![0; self.part_size];
+
+            match file.read(&mut buffer) {
+                Ok(len) => {
+                    if len == 0 {
+                        info!("Done!");
+                        break;
+                    }
+                    buffer.truncate(len);
+                    match self.upload_part(buffer, filename, part_number, upload_id) {
+                        Ok(completed_part) => {
+                            completed_parts.push(completed_part);
+                            part_number += 1;
+                        },
+                        Err(err) => {
+                            return Err(err);
+                        }
+                    }
+                },
+                Err(err) => {
+                    error!("Error reading file: {}", err);
+                    return Err(Error::Read(err));
+                }
+            }
+        }
+
+        Ok(CompletedMultipartUpload{parts: Some(completed_parts)})
+    }
+
+    fn upload_part(&self,
+                   body: Vec<u8>,
+                   filename: &str,
+                   part_number: i64,
+                   upload_id: &str) -> Result<CompletedPart> {
+        return match self.s3_client.upload_part(
+            UploadPartRequest {
+                body: Some(body.into()),
+                bucket: self.bucket_name.to_owned(),
+                key: filename.to_owned(),
+                part_number,
+                upload_id: upload_id.to_owned(),
+                ..Default::default()
+            }).sync() {
+            Ok(res) => {
+                let e_tag = res.e_tag.unwrap();
+                info!("Uploaded part {} - etag: {}", part_number, e_tag);
+                Ok(CompletedPart {
+                    part_number: Some(part_number),
+                    e_tag: Some(e_tag)
+                })
+            },
+            Err(err) => {
+                error!("Failed to upload part {}: {}", part_number, err);
+                Err(Error::UploadPart)
+            }
+        }
+    }
+
+    fn complete_multipart_upload(&self,
+                                 filename: &str,
+                                 multipart_upload: CompletedMultipartUpload,
+                                 upload_id: &str) -> Result<()> {
+            match self.s3_client.complete_multipart_upload(
+                CompleteMultipartUploadRequest {
+                    bucket: self.bucket_name.to_owned(),
+                    key: filename.to_owned(),
+                    multipart_upload: Some(multipart_upload),
+                    upload_id: upload_id.to_owned(),
+                    request_payer: self.request_payer.to_owned(),
+                }
+            ).sync() {
+                Ok(_) => {
+                    info!("Completed upload");
+                    Ok(())
+                },
+                Err(err) => {
+                    error!("Failed to complete upload: {}", err);
+                    Err(Error::CompleteMultipartUpload)
+                }
+            }
+    }
+
+    fn create_multipart_upload(&self, filename: &str) -> Result<String> {
+        match self.s3_client.create_multipart_upload(
+            CreateMultipartUploadRequest {
+                bucket: self.bucket_name.clone(),
+                key: filename.to_owned(),
+                ..Default::default()
+            }).sync()
+            {
+                Ok(result) => {
+                    match result.upload_id {
+                        Some(upload_id) => Ok(upload_id),
+                        None => Err(Error::CreateMultipartUpload)
+                    }
+                },
+                Err(err) => {
+                    error!("Failed to create multipart upload for {}: {}", filename, err);
+                    Err(Error::CreateMultipartUpload)
+                }
+            }
+    }
+
+    fn abort_multipart_upload(&self, filename: &str, upload_id: &str) {
+        match self.s3_client.abort_multipart_upload(AbortMultipartUploadRequest {
+            bucket: self.bucket_name.to_owned(),
+            key: filename.to_owned(),
+            upload_id: upload_id.into(),
+            request_payer: self.request_payer.to_owned(),
+        }).sync() {
+            Ok(_) => warn!("Aborted upload of {} (upload id: {})", filename, upload_id),
+            Err(err) => warn!("Failed to abort upload of {} (upload id: {}): {}", filename, upload_id, err),
         }
     }
 }
+
+pub type Result<T> = StdResult<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    CreateMultipartUpload,
+    UploadPart,
+    CompleteMultipartUpload,
+    Read(IOError),
+}
+
