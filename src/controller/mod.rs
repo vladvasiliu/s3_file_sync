89c92e16309bdb0a095de9d138e988c76d7546c8
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2020-01-27 16:27:13 +0100

    Moved back to standard threads to improve error handling.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,19 +1,15 @@
 use std::path::Path;
-use std::sync::mpsc;
+use std::thread::Builder;
+
 use crossbeam_channel::unbounded;
-use crossbeam_utils::thread;
-
 use log::{warn, info};
-
 
 pub mod error;
 pub mod file;
 
-use crate::controller::file::File;
 use crate::controller::error::{Result};
 use crate::uploader::Uploader;
 use crate::watcher::FileWatcher;
-use std::thread::Builder;
 
 
 pub struct Controller {
@@ -23,27 +19,26 @@ impl Controller {
     pub fn run<P: AsRef<Path>>(paths: &[P], duration: u64) -> Result<()> {
         let (watcher_tx, watcher_rx) = unbounded();
         let num_uploaders = 2;
-        let uploader = Uploader::new("test-s3-file-sync", "eu-west-3");
 
-        thread::scope(|s| {
-            for num in 1..=num_uploaders {
-                s.builder().name(format!("uploader {}", num)).spawn(|_| uploader.run()).unwrap();
-            }
+        let mut uploaders = vec![];
+        let mut watchers = vec![];
 
-            for watcher in FileWatcher::create_watchers(paths, watcher_tx, duration).unwrap() {
-                s.builder().name(watcher.base_path.display().to_string()).spawn(move |_| watcher.run()).unwrap();
-            }
+        for num in 1..=num_uploaders {
+            let uploader = Uploader::new("test-s3-file-sync", "eu-west-3");
+            uploaders.push(Builder::new().name(format!("uploader {}", num)).spawn(move || uploader.run())?);
+        }
 
-            loop {
-                match watcher_rx.recv() {
-                    Err(err) => warn!("Failed to receive file from watcher: {}", err),
-                    Ok(file) => {
-                        info!("Received file from watcher: {}", file);
-                    }
+        for watcher in FileWatcher::create_watchers(paths, watcher_tx, duration).unwrap() {
+            watchers.push(Builder::new().name(watcher.base_path.display().to_string()).spawn(move || watcher.run())?);
+        }
+
+        loop {
+            match watcher_rx.recv() {
+                Err(err) => warn!("Failed to receive file from watcher: {}", err),
+                Ok(file) => {
+                    info!("Received file from watcher: {}", file);
                 }
             }
-        });
-
-        Ok(())
+        }
     }
 }
