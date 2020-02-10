a29b2e3989185dc0715a6bf59107c7cd6d769ef0
Vlad Vasiliu <vladvasiliun@yahoo.fr>
2020-02-10 17:20:52 +0100

    Started work on command line parser.

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,5 @@
 use clap::{App, Arg, ArgMatches};
 use log::info;
-use std::collections::HashSet;
 use std::f32::MAX;
 
 static DEFAULT_UPLOAD_SIZE: u64 = 100;
@@ -11,14 +10,14 @@ static DEFAULT_WATCHER_INTERVAL: u64 = 2;
 static MIN_WATCHER_INTERVAL: u64 = 1;
 
 pub struct Config {
-    watched_dirs: HashSet<String>,
+    watched_dirs: Vec<String>,
     bucket_name: String,
-    num_uploaders: usize,
-    upload_part_size: usize,
+    num_uploaders: u64,
+    upload_part_size: u64,
 }
 
 impl Config {
-    pub fn from_args() {
+    pub fn from_args() -> Self {
         let upload_size_default = format!("{}", DEFAULT_UPLOAD_SIZE);
         let watcher_interval_default = format!("{}", DEFAULT_WATCHER_INTERVAL);
         let uploader_threads_default = format!("{}", DEFAULT_NUM_UPLOADERS);
@@ -86,6 +85,21 @@ impl Config {
                     .validator(int_gte_1),
             )
             .get_matches();
+
+        Self {
+            watched_dirs: matches
+                .values_of("watch_dir")
+                .unwrap()
+                .map(|e| e.into())
+                .collect(),
+            bucket_name: matches.value_of("bucket_name").unwrap().into(),
+            num_uploaders: matches
+                .value_of("uploader_threads")
+                .unwrap()
+                .parse()
+                .unwrap(),
+            upload_part_size: matches.value_of("upload_size").unwrap().parse().unwrap(),
+        }
     }
 }
 
